640965eaa342f98aee4b2f83a0c6492099b6a461
Vincent Ahrens <vincent.ahrens@gmx.net>
2022-02-15 11:36:08 +0100

    error

--- src/test/java/de/neuefische/citest/CiTestApplicationTests.java
+++ src/test/java/de/neuefische/citest/CiTestApplicationTests.java
@@ -1,5 +1,6 @@
 package de.neuefische.citest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class CiTestApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail();
     }
 
 }
